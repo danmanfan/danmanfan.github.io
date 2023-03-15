1fb43244c4f3e205f3df989d2daba9c3e1bfa587
Daniel J Flynn <danielflynn24@yahoo.com>
2023-03-15 00:19:49 -0700

    added sliders in svg paint
    sliders and a color block were added to SVG_Paint

--- SVG_Paint/JSSVGPaint.js
+++ SVG_Paint/JSSVGPaint.js
@@ -1,8 +1,16 @@
 
 
 let svg_paint = document.getElementById("svg_paint");
-let svg_color_wheel = document.getElementById("svg_color_wheel");
-let svg_ctx = svg_color_wheel.getContext("2d");
+// let svg_color_wheel = document.getElementById("svg_color_wheel");
+let red_slider = document.getElementById("red_slider");
+let green_slider = document.getElementById("green_slider");
+let blue_slider = document.getElementById("blue_slider");
+
+let color_block = document.getElementById("color_block");
+let color_block_ctx = color_block.getContext("2d");
+
+// let svg_ctx = svg_color_wheel.getContext("2d");
+
 
 // Have buttons selectable such that: if circle is selected, user begins drawing a circle etc.
 
@@ -51,25 +59,27 @@ let js_svg_path_tail = " \" stroke=" + svg_stroke + "stroke-width\"3\" fill=\"no
 document.addEventListener("mousemove", mouseMoveHandler, false);
 document.addEventListener("mousedown", mouseDownHandler, false);
 document.addEventListener("mouseup", mouseUpHandler, false);
-document.addEventListener("ontouchstart", mouseDownHandler, false);
-document.addEventListener("ontouchmove", mouseMoveHandler, false);
+// document.addEventListener("ontouchstart", mouseDownHandler, false);
+// document.addEventListener("ontouchmove", mouseMoveHandler, false);
 
 
 
 function mouseDownHandler(e) {
-    let svg_color_wheel_rect = svg_color_wheel.getBoundingClientRect();
-    let svg_color_rel_x = e.clientX - svg_color_wheel_rect.left;
-    let svg_color_rel_y = e.clientY - svg_color_wheel_rect.top;
-    if(svg_color_rel_x > 255 && svg_color_rel_x < 512) {
-        r_paint = svg_color_rel_x - 255;
-        b_paint = 0;
-    } else if(svg_color_rel_x > -1 && svg_color_rel_x < 256) {
-        r_paint = 0;
-        b_paint = 256 - svg_color_rel_x;
-    }
-    if(svg_color_rel_y > -1 && svg_color_rel_y < 256) {
-        g_paint = svg_color_rel_y;
-    }
+    // let svg_color_wheel_rect = svg_color_wheel.getBoundingClientRect();
+    // let svg_color_rel_x = e.clientX - svg_color_wheel_rect.left;
+    // let svg_color_rel_y = e.clientY - svg_color_wheel_rect.top;
+    // if(svg_color_rel_x > 255 && svg_color_rel_x < 512) {
+    //     r_paint = svg_color_rel_x - 255;
+    //     b_paint = 0;
+    // } else if(svg_color_rel_x > -1 && svg_color_rel_x < 256) {
+    //     r_paint = 0;
+    //     b_paint = 256 - svg_color_rel_x;
+    // }
+    // if(svg_color_rel_y > -1 && svg_color_rel_y < 256) {
+    //     g_paint = svg_color_rel_y;
+    // }
+
+
     svg_stroke = "\"rgb(" + r_paint + ", " + g_paint + ", " + b_paint + ")\""
     js_svg_path_tail = " \" stroke=" + svg_stroke + "stroke-width\"3\" fill=\"none\"/>";
     js_svg_path = js_svg_path + "<path d=\"" + js_svg_path_M + js_svg_path_tail;
@@ -97,40 +107,18 @@ function mouseMoveHandler(e) {
     }
 }
 
+function colorInBlock() {
+    r_paint = red_slider.value;
+    g_paint = green_slider.value;
+    b_paint = blue_slider.value;
 
-function colorSelector() {
-
+    color_block_ctx.beginPath();
+    color_block_ctx.fillStyle = "rgb(" + r_paint + ", " + g_paint + ", " + b_paint + ")";
+    color_block_ctx.rect(0, 0, color_block.width, color_block.height);
+    color_block_ctx.fill();
+    color_block_ctx.closePath();
+    requestAnimationFrame(colorInBlock);
 }
 
 
-function colorTriangle() {
-    
-    for(let i = 0; i < 511; i ++) {
-        for(let j = 0; j < 256; j++) {
-            svg_ctx.beginPath();
-            svg_ctx.rect(i, j, 1, 1);
-            if(i > 255) {
-                r = i - 256;
-                b = 0;
-            } else {
-                r = 0;
-                b = 255 - i;
-            }
-            g = j;
-            svg_ctx.fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
-            svg_ctx.fill();
-            svg_ctx.closePath()
-        }
-    }
-}
-
-
-colorTriangle();
-// function JSSVGPaintMain() {
-
-
-//     // requestAnimationFrame(JSSVGPaintMain);    
-// }
-
-
-// JSSVGPaintMain();
+colorInBlock();
