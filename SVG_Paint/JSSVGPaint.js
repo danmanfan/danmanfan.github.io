e3cd31fcb9cb6688103697fa5e2d075bb8094c8e
Daniel J Flynn <danielflynn24@yahoo.com>
2023-04-05 10:28:02 -0700

    Squares!
    Squares!

--- SVG_Paint/JSSVGPaint.js
+++ SVG_Paint/JSSVGPaint.js
@@ -2,6 +2,8 @@
 
 let svg_paint = document.getElementById("svg_paint");
 let file_name = document.getElementById('file_name');
+let line_button = document.getElementById("line_button");
+let square_button = document.getElementById("square_button");
 
 // let svg_color_wheel = document.getElementById("svg_color_wheel");
 let red_slider = document.getElementById("red_slider");
@@ -16,26 +18,59 @@ let valid_file_name = false;
 
 // Have buttons selectable such that: if circle is selected, user begins drawing a circle etc.
 
+// relative x position
 let js_svg_rel_x = 0;
+// relative y position
 let js_svg_rel_y = 0;
 
-let r = 0;
-let g = 0;
-let b = 0;
 
+// let r = 0;
+// let g = 0;
+// let b = 0;
+
+// stroke colors
 let r_paint = 0;
 let g_paint = 0;
 let b_paint = 0;
 
-let svg_stroke = "\"rgb(" + r_paint + ", " + g_paint + ", " + b_paint + ")\""
 
+let refresh_point = 0;
+
+// text inside of a svg file
+let js_svg_obj = "";
+
+
+let svg_stroke = "\"rgb(" + r_paint + ", " + g_paint + ", " + b_paint + ")\""
+// the x position of svg object
+let x_pos = 0;
+// the y position of svg object
+let y_pos = 0;
 // Rectangle <rect>
+let svg_rect = false;
+svg_rect = true; // testing
+let js_svg_rect = "<rect"
+// "<rect width=\"300\" height=\"100\" style=\"fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)\" />";
+let js_svg_rect_x = " x=\"0\"";
+let js_svg_rect_y = " y=\"0\"";
+let js_svg_rect_width = " width=\"0\"";
+let js_svg_rect_height = " height=\"0\"";
+let js_svg_rect_style = " style=";
+// Might me used for others:
+let js_svg_rect_fill = "\"fill:none";
+let js_svg_rect_stroke_width = ";stroke-width:3";
+let js_svg_rect_stroke = ";stroke:rgb(0,0,0)\"";
+
+
+
 // Circle <circle>
 // Ellipse <ellipse>
 // Line <line>
 // Polyline <polyline>
 // Polygon <polygon>
 // Path <path>
+let svg_path = true; // path is true by default
+svg_path = false; // testing
+
 let js_svg_path_M = "M 0 0";
 let js_svg_path = "";
 let pen_down = false;
@@ -68,29 +103,69 @@ document.addEventListener("mouseup", mouseUpHandler, false);
 
 
 function mouseDownHandler(e) {
+    
     svg_stroke = "\"rgb(" + r_paint + ", " + g_paint + ", " + b_paint + ")\""
-    js_svg_path_tail = " \" stroke=" + svg_stroke + "stroke-width=\"1\" fill=\"none\"/>";
-    js_svg_path = js_svg_path + "<path d=\"" + js_svg_path_M + js_svg_path_tail;
+    
+    if(svg_path) {
+        
+        js_svg_path_tail = " \" stroke=" + svg_stroke + "stroke-width=\"1\" fill=\"none\"/>";
+        js_svg_obj = js_svg_obj + "<path d=\"" + js_svg_path_M + js_svg_path_tail;
+        // js_svg_path = js_svg_path + "<path d=\"" + js_svg_path_M + js_svg_path_tail;
+    } else if (svg_rect) {
+
+        
+    }
+    
     pen_down = true;
+
 }
 
 function mouseUpHandler(e) {
+    if(svg_rect) {
+        if(pen_down) {
+            svg_paint.innerHTML = svg_paint.innerHTML + js_svg_obj;
+        }
+        
+    }
+    refresh_point = js_svg_obj.length;
     pen_down = false;
 }
 
 function mouseMoveHandler(e) {
-    let js_svg_path_end = js_svg_path.length - js_svg_path_tail.length;
+    // let js_svg_path_end = js_svg_path.length - js_svg_path_tail.length;
     let svg_paint_rect = svg_paint.getBoundingClientRect();
     js_svg_rel_x = e.clientX - svg_paint_rect.left;
     js_svg_rel_y = e.clientY - svg_paint_rect.top;
     if(pen_down) {
         
-        
+        if(svg_path) {
+            drawPath();
+        } else if (svg_rect) {
+            let temp_x = x_pos;
+            let temp_y = y_pos;
+            let temp_width = js_svg_rel_x - x_pos;  
+            let temp_height = js_svg_rel_y - y_pos;
+            if(temp_width < 0) {
+                temp_width = -temp_width;
+                temp_x = js_svg_rel_x;
+            }
+            if(temp_height < 0) {
+                temp_height = -temp_height;
+                temp_y = js_svg_rel_y;
+            }
+            js_svg_rect_width = js_svg_rect_width.substring(0,7) + "\"" + temp_width + "\"";
+            js_svg_rect_height = js_svg_rect_height.substring(0,8) + "\"" + temp_height + "\"";
 
-        let points = "L" + js_svg_rel_x + " " + js_svg_rel_y;
-        js_svg_path = js_svg_path.substring(0, js_svg_path_end) + points + js_svg_path_tail;
-        svg_paint.innerHTML = js_svg_path;
+            let temp_js_svg_obj = js_svg_rect + " x=" + "\"" + temp_x + "\"" + " y=" + "\"" + temp_y + "\""
+            + js_svg_rect_width + js_svg_rect_height + js_svg_rect_style + js_svg_rect_fill + js_svg_rect_stroke_width 
+            + js_svg_rect_stroke.substring(0,12) + r_paint + ", " + g_paint + ", " + b_paint + ")\"" + "/>";
+            js_svg_obj = js_svg_obj.substring(0, refresh_point) + temp_js_svg_obj;
+            svg_paint.innerHTML = js_svg_obj; // delete previous square
+
+        }
     } else {
+        x_pos = js_svg_rel_x;
+        y_pos = js_svg_rel_y;
         js_svg_path_M = "M" + " " + js_svg_rel_x + " " + js_svg_rel_y;
     }
 }
@@ -161,6 +236,38 @@ function downloadSVG() {
 function jsSVGPaintRunner() {
     colorInBlock();
 }
+
+function drawPath() {
+    // let js_svg_path_end = js_svg_path.length - js_svg_path_tail.length;
+    let js_svg_path_end = js_svg_obj.length - js_svg_path_tail.length;
+    let points = "L" + js_svg_rel_x + " " + js_svg_rel_y;
+    js_svg_obj = js_svg_obj.substring(0, js_svg_path_end) + points + js_svg_path_tail;
+    // js_svg_path = js_svg_path.substring(0, js_svg_path_end) + points + js_svg_path_tail;
+    svg_paint.innerHTML = js_svg_obj;
+}
+
+
+
+//Button calls
+
+function lineOnClick() {
+    // line_button.setAttribute(disabled);
+    // square_button.setAttribute(!disabled);
+    svg_path = true;
+    svg_rect = false;
+}
+
+function squareOnClick() {
+    // square_button.setAttribute(disabled);
+    // line_button.setAttribute(!disabled);
+    svg_path = false;
+    svg_rect = true;
+}
+
+
+
+// Access google drive
+// https://developers.google.com/drive/api/guides/about-sdk
 
 
 
